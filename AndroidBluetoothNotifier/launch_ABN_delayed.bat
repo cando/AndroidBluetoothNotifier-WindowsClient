4ee7c839e8540777355ec1607a9851a5a2aa7d77
Stefano Candori <kendrix16@yahoo.it>
2013-11-26 08:26:28 +0100

    Privacy concerns on .bat file.

--- AndroidBluetoothNotifier/launch_ABN_delayed.bat
+++ AndroidBluetoothNotifier/launch_ABN_delayed.bat
@@ -1,3 +1,3 @@
 @echo off
 timeout 5
-start "" "C:\Users\candoris\Documents\Visual Studio 2013\Projects\AndroidBluetoothNotifier\AndroidBluetoothNotifier\bin\Release\AndroidBluetoothNotifier.exe"
+start "" "AndroidBluetoothNotifier.exe"
